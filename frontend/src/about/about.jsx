2a7738f4e16535a66185285577d2ed5a99a9b0e2
melqui-andrade <melqui.inbox@gmail.com>
2018-09-06 09:11:00 -0300

    Defined aplication routers, menu, and header

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import PageHearder from '../template/pageHeader'
+
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHearder name='Sobre' small='Nós'></PageHearder>
+
+        <h2>Nossa História</h2>
+        <p>Mauris enim leo, condimentum et facilisis nec, fringilla id tellus. Donec interdum vulputate metus, eu varius orci. Quisque ut tempor libero, ac semper eros.</p>
+        <h2>Missão e Visão</h2>
+        <p>Proin eros ex, aliquet et dui vel, accumsan ullamcorper lorem.</p>
+        <h2>Imprensa</h2>
+        <p>Quisque in lorem venenatis, imperdiet nunc a, placerat lectus.</p>
     </div>
 )
