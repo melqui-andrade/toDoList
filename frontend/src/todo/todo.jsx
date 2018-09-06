0f347a4b8fa79a0d76443c0acd01a54f2e6c9d1d
melqui-andrade <melqui.inbox@gmail.com>
2018-09-06 11:59:14 -0300

    Created table with todo elements. Add and remove actions implemented.

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,12 +1,50 @@
 import React, {Component} from 'react'
 
 import PageHeader from '../template/pageHeader'
+import TodoForm from '../todo/todoForm'
+import TodoList from '../todo/todoList'
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component{
+    constructor(props){
+        super(props)
+        this.state={description: '', list:[]}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleRemove = this.handleRemove.bind(this)
+        this.refresh()
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(resp => this.setState({...this.state, description: '', list: resp.data}))
+    }
+
+    handleChange(e){
+        this.setState({...this.state, description: e.target.value})
+    }
+
+    handleAdd(){
+        const description = this.state.description
+        axios.post(URL, {description})
+            .then(resp => this.refresh())
+    }
+
+    handleRemove(todo){
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => this.refresh())
+    }
+    
     render(){
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm description={this.state.description} 
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}/>
+                <TodoList list={this.state.list} handleRemove={this.handleRemove} />
             </div>
         )
     }
